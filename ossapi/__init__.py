6d9c35ec97d2ca6b5256f024fa5428f535a0324c
Liam DeVoe <orionldevoe@gmail.com>
2021-03-16 12:15:42 -0400

    add Mod to ossapi (moved from circlecore)

--- ossapi/__init__.py
+++ ossapi/__init__.py
@@ -2,9 +2,10 @@ import logging
 
 from ossapi.ossapi import Ossapi
 from ossapi.ossapiv2 import OssapiV2
+from ossapi.mod import Mod
 from ossapi.version import __version__
 
-__all__ = ["Ossapi", "OssapiV2", "__version__"]
+__all__ = ["Ossapi", "OssapiV2", "__version__", "Mod"]
 
 # we need to explicitly set a handler for the logging module to be happy
 handler = logging.StreamHandler()
