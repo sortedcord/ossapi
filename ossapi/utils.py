db92fa54832200d56eebc03724500a0c75d4c1fb
Liam DeVoe <orionldevoe@gmail.com>
2021-03-17 22:56:31 -0400

    clean up ListEnumMeta init

--- ossapi/utils.py
+++ ossapi/utils.py
@@ -5,13 +5,22 @@ class ListEnumMeta(EnumMeta):
     Allows an enum to be instantiated with a list of members of the enum. So
     `PlayStyles([1, 8])` is equivalent to `PlayStyles.MOUSE | PlayStyles.TOUCH`.
     """
-    def __call__(cls, value, names=None, *, module=None, qualname=None, type=None, start=1):
+    def __call__(cls, value, names=None, *, module=None, qualname=None,
+        type=None, start=1):
+
+        def _instantiate(value):
+            # interestingly, the full form of super is required here (instead of
+            # just ``super().__call__``). I guess it's binding to this inner
+            # method instead of the class?
+            return super(ListEnumMeta, cls).__call__(value, names,
+                module=module, qualname=qualname, type=type, start=start)
+
         if not isinstance(value, list):
-            return super().__call__(value, names, module=module, qualname=qualname, type=type, start=start)
+            return _instantiate(value)
         value = iter(value)
         val = next(value)
-        new_val = super().__call__(val, names, module=module, qualname=qualname, type=type, start=start)
+        new_val = _instantiate(val)
         for val in value:
-            val = super().__call__(val, names, module=module, qualname=qualname, type=type, start=start)
+            val = _instantiate(val)
             new_val |= val
         return new_val
