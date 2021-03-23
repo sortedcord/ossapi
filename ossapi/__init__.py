aec5f022e1d9695ac0ba6cbe8b21a08977be1dfb
Liam DeVoe <orionldevoe@gmail.com>
2021-03-23 17:32:56 -0400

    export everything from enums and models in init

--- ossapi/__init__.py
+++ ossapi/__init__.py
@@ -5,7 +5,12 @@ from ossapi.ossapiv2 import OssapiV2
 from ossapi.mod import Mod
 from ossapi.version import __version__
 
-__all__ = ["Ossapi", "OssapiV2", "__version__", "Mod"]
+# TODO turn these back to explicit imports when ossapi (and api v2 itself) has
+# stabilized a little bit
+from ossapi.enums import *
+from ossapi.models import *
+
+# __all__ = ["Ossapi", "OssapiV2", "__version__", "Mod"]
 
 # we need to explicitly set a handler for the logging module to be happy
 handler = logging.StreamHandler()
