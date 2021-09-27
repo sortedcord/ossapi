a2433131c3b9009ce200a6ea2700d29966f35e91
Liam DeVoe <orionldevoe@gmail.com>
2021-09-26 21:46:38 -0400

    fix unittest api instantiation

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,4 @@
-from ossapi import OssapiV2, Ossapi
+from ossapi import OssapiV2, Ossapi, Grant
 
 import os
 
@@ -12,5 +12,5 @@ if not client_secret:
     client_secret = input("Enter your api v2 client secret: ")
 
 client_id = int(client_id)
-api = OssapiV2(client_id, client_secret, strict=True)
+api = OssapiV2(Grant.CLIENT_CREDENTIALS, client_id, client_secret, strict=True)
 apiv1 = Ossapi(key)
