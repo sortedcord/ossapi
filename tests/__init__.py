fd637192341d86776f5ef145f98eab95bfabb311
Liam DeVoe <orionldevoe@gmail.com>
2021-10-07 22:52:08 -0400

    fix tests api instantiation

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,5 +12,6 @@ if not client_secret:
     client_secret = input("Enter your api v2 client secret: ")
 
 client_id = int(client_id)
-api = OssapiV2(Grant.CLIENT_CREDENTIALS, client_id, client_secret, strict=True)
+api = OssapiV2(client_id, client_secret, strict=True,
+    grant=Grant.CLIENT_CREDENTIALS)
 apiv1 = Ossapi(key)
