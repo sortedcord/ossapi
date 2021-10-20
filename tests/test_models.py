7c8eac217172f77d9498b97b33e0f9e2443375be
Liam DeVoe <orionldevoe@gmail.com>
2021-10-19 20:21:42 -0400

    add some foreign key tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
 
+from ossapi import User, BeatmapsetCompact, UserCompact, GameMode
+
 from tests import api
+
 
 class TestMethodTypeConversion(TestCase):
     def test_beatmap_as_parameter(self):
@@ -36,3 +39,71 @@ class TestExpandableModels(TestCase):
     # TODO add test_expand_beatmap when I find a good endpoint that returns
     # BeatmapCompacts and not full Beatmaps.
 
+class TestFollowingForeignKeys(TestCase):
+    def test_beatmap_fks(self):
+        beatmap = api.beatmap(221777)
+
+        user = beatmap.user()
+        self.assertIsInstance(user, User)
+        self.assertEqual(user.id, 1047883)
+
+        beatmapset = beatmap.beatmapset()
+        self.assertIsInstance(beatmapset, BeatmapsetCompact)
+        self.assertEqual(beatmapset.id, 79498)
+        self.assertEqual(beatmapset.user_id, 1047883)
+
+    def test_beatmapset_fks(self):
+        beatmapset = api.beatmapset(beatmap_id=3207950)
+
+        user = beatmapset.user()
+        self.assertIsInstance(user, UserCompact)
+        self.assertEqual(user.id, 4693052)
+
+    def test_score_fks(self):
+        score = api.score(GameMode.STD, 3685255338)
+
+        user = score.user()
+        self.assertIsInstance(user, UserCompact)
+        self.assertEqual(user.id, 12092800)
+
+    def test_comment_fks(self):
+        comment = api.comment(1533934).comments[0]
+
+        user = comment.user()
+        self.assertIsInstance(user, User)
+        self.assertEqual(user.id, 12092800)
+
+        edited_by = comment.edited_by()
+        self.assertIsNone(edited_by)
+
+    def test_forum_post_fks(self):
+        post = api.forum_topic(141240).posts[0]
+
+        user = post.user()
+        self.assertIsInstance(user, User)
+        self.assertEqual(user.id, 2)
+
+        edited_by = post.edited_by()
+        self.assertIsNone(edited_by)
+
+    def test_forum_topic_fks(self):
+        topic = api.forum_topic(141240).topic
+
+        user = topic.user()
+        self.assertIsInstance(user, User)
+        self.assertEqual(user.id, 2)
+
+    def test_beatmapset_discussion_post_fks(self):
+        # https://osu.ppy.sh/beatmapsets/1576285/discussion#/2641058
+        bmset_disc_post = api.beatmapset_discussion_posts(2641058).posts[0]
+
+        user = bmset_disc_post.user()
+        self.assertIsInstance(user, User)
+        self.assertEqual(user.id, 6050530)
+
+        last_editor = bmset_disc_post.last_editor()
+        self.assertIsInstance(last_editor, User)
+        self.assertEqual(last_editor.id, 6050530)
+
+        deleted_by = bmset_disc_post.deleted_by()
+        self.assertIsNone(deleted_by)
